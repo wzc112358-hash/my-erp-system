0959bd7f76f3100bd2ec1f8758863640338bc43c
wzc112358-hash <wzc112358-hash@users.noreply.github.com>
2026-05-09 16:38:56 +0800

    修复了一些偶发性上传失败的问题

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
